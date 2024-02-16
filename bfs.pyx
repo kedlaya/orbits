1a2bd5f1f4df5520561896d6c01a5538a92c7835
Kiran S. Kedlaya <kskedl@gmail.com>
2024-02-16 15:16:42 -0800

    Switch search from breadth-first to depth-first

--- bfs.pyx
+++ bfs.pyx
@@ -1,19 +1,15 @@
-from collections import deque
-
 def bfs(neighbors, dict d, v0):
-    cdef long c = 1
+    cdef long c = 0
     cdef long count = 1
-    queue = deque()
-    queue.append((v0, d[v0]))
-    while c:
-        w, t = queue.popleft()
-        c -= 1
+    cdef list queue = [(v0, d[v0])]
+    while c < count:
+        c += 1
+        w, t = queue.pop()
         for x, g in neighbors(w):
             if x not in d:
                 u = (t[0], g*t[1])
                 d[x] = u
                 queue.append((x, u))
-                c += 1
                 count += 1
     return count
 
